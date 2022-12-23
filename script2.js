9b6fb2edaba10bb722a08ea03ac6a7a691500fac
rockwillck <38663064+rockwillck@users.noreply.github.com>
2022-12-22 21:13:08 -0600

    Completely redid carousel system, fixed image bug.

--- script2.js
+++ script2.js
@@ -8,67 +8,73 @@ images/IMG_0233.jpeg
 images/IMG_0259.jpeg
 images/IMG_2229.jpeg
 images/IMG_3479.jpeg
-images/IMG_3543.jpeg
 images/IMG_3645.jpeg
 images/IMG_3672.jpeg
-images/IMG_4076.jpeg`
-bgs = bgString.split(`
+images/IMG_4076.jpeg
+images/Screen Shot 2022-12-22 at 9.10.08 PM.png`
+var bgs = bgString.split(`
 `)
-backgrounds = []
-c = 0
-bgs.forEach((b) => {
-    backgrounds.push(b)
-    image = new Image()
-    image.src = b
-    image.onload = () => {
-        if (canvas2.width/canvas2.height > image.width/image.height) {
-            imageW = canvas2.width
-            imageH = imageW*image.height/image.width
-        } else {
-            imageH = canvas2.height
-            imageW = imageH*image.width/image.height
+
+function generateList() {
+    let c = 0
+    let backgrounds = []
+    for (i=0;i<bgs.length;i++) {
+        b = bgs[i]
+        let image = new Image()
+        image.src = b
+        image.onload = () => {
+            let imageW = canvas2.height*image.width/image.height
+            backgrounds.push([image, c, imageW])
+            c += imageW
         }
-        c += imageW
     }
-})
-offset = 0
+
+    return backgrounds
+}
+
+var carousel = generateList()
 function animate2() {
     requestAnimationFrame(animate2)
     ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
 
-    current = 0
-    backgrounds.forEach((bg) => {
-        image = new Image()
-        image.src = bg
-        if (canvas2.width/canvas2.height > image.width/image.height) {
-            imageW = canvas2.width
-            imageH = imageW*image.height/image.width
-        } else {
-            imageH = canvas2.height
-            imageW = imageH*image.width/image.height
+    speed = canvas2.width/100
+    carousel.forEach((bg) => {
+        if ((bg[1] >= 0 && bg[1] + bg[2] <= canvas2.width) || (bg[1] <= 0 && bg[1] + bg[2] >= canvas2.width)) {
+            speed = canvas2.width/500
         }
-        ctx2.drawImage(image, current + offset, canvas2.height/2 - imageH/2, imageW, imageH)
-        current += imageW
     })
-
-    offset -= 10
-    if (Math.abs(offset + current) <= 10 + imageW) {
-        bgs.forEach((b) => {
-            backgrounds.push(b)
-        })
-    }
-
-    if (offset + 2*c <= 10) {
-        offset += c
-        backgrounds.splice(backgrounds.length - 1 - bgs.length, bgs.length)
-    }
+    carousel.forEach((bg, index) => {
+        ctx2.drawImage(bg[0], bg[1], 0, bg[2], canvas2.height)
+        bg[1] -= speed
+        if (bg[1] + bg[2] < 0) {
+            carousel.splice(index, 1)
+            bg[1] = carousel[carousel.length-1][1] + carousel[carousel.length-1][2]
+            carousel.push(bg)
+        }
+        // ctx.lineWidth = 10
+        // ctx.beginPath()
+        // ctx.moveTo(bg[1], 0)
+        // ctx.lineTo(bg[1], canvas2.height)
+        // ctx.closePath()
+        // ctx.stroke()
+        // ctx.beginPath()
+        // ctx.moveTo(bg[1] + bg[2], 0)
+        // ctx.lineTo(bg[1] + bg[2], canvas2.height)
+        // ctx.closePath()
+        // ctx.stroke()
+    })
+    // img = new Image()
+    // img.src = "images/Screen Shot 2022-12-22 at 9.10.08 PM.png"
+    // // img.onload = () => {
+    //     ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height)
+    // // }
 }
+animate2()
 
 function resize() {
     canvas2.height = window.innerHeight
     canvas2.width = window.innerWidth
     canvas.height = window.innerHeight
     canvas.width = window.innerWidth
+    carousel = generateList()
 }
-
-animate2()
